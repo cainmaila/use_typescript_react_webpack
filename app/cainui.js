ad9e099cbc2de5324f79ecdb1acafc1db78541e7
Cain.Chu <cainmaila@gmail.com>
2016-01-21 18:55:02 +0800

    換頁事件

--- app/cainui.js
+++ app/cainui.js
@@ -5,16 +5,25 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var React = require('react');
+var react_redux_1 = require('react-redux');
 var Link = require('react-router').Link;
+var routeActions = require('redux-simple-router').routeActions;
 var CainUi = (function (_super) {
     __extends(CainUi, _super);
     function CainUi(props) {
         _super.call(this, props);
     }
     CainUi.prototype.render = function () {
-        return React.createElement("div", {className: "cainui"}, React.createElement(Link, {to: "/"}, "XXXXXXX"), React.createElement("p", null, "Cain UI !!"));
+        setTimeout(this.funA.bind(this), 1000);
+        return React.createElement("div", {className: "cainui"}, React.createElement(Link, {to: "/"}, "XXXXXXX"), React.createElement("p", null, "Cain UI !! ", this.props.initialCount));
+    };
+    CainUi.prototype.funA = function () {
+        this.props.dispatch(routeActions.push("/"));
     };
     return CainUi;
 })(React.Component);
+function select(state) {
+    return state;
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = CainUi;
+exports.default = react_redux_1.connect(select)(CainUi);
--- app/cainui.tsx
+++ app/cainui.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
+import { connect  } from 'react-redux';
 var Link = require('react-router').Link;
-export default class CainUi extends React.Component<any,any> {
+var routeActions = require('redux-simple-router').routeActions;
+class CainUi extends React.Component<any,any> {
     constructor(props) {
         super(props);
     }
     render() {
+        // console.log(this.props)
+        setTimeout(this.funA.bind(this),1000);
         return <div className="cainui" >
             <Link to="/">XXXXXXX</Link>
-            <p>Cain UI !!</p>
+            <p>Cain UI !! {this.props.initialCount}</p>
         </div>
     }
+    funA(){
+        this.props.dispatch(routeActions.push("/")); //換頁事件
+    }
 }
+function select(state) {
+    return state;
+}
+export default connect(
+    select
+)(CainUi);
